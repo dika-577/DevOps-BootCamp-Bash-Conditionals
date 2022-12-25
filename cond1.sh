f9e2653aa74e350ffc063020b0889bcb8ce94225
dimitri <korgalidzed@gmail.com>
2022-12-25 12:37:28 +0400

    change in cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -3,11 +3,11 @@
 IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
-arr=(${1//,/ })
+
 
 sum=0
 
-for i in "${arr[@]}"; do
+for i in "${array[@]}"; do
   if [[ $(($i % 2)) == 0 ]]; then
     sum=$(($sum + $i))
   fi
